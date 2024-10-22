1db01ca2258d9e8634019ba26cafa7f97ccffa3f
Baptiste Nouailhac <baptiste.nouailhac@epitech.eu>
2024-10-22 17:29:36 +0200

    Update misp_conf.py

--- src/conf/misp_conf.py
+++ src/conf/misp_conf.py
@@ -1,5 +1,5 @@
 # Configuration for MISP instance
 misp_url = 'https://192.168.215.128/'
 # key publisher :
-misp_user_key = 'here' # API key for MISP
+misp_user_key = 'WriteKeysHere' # API key for MISP
 misp_verifycert = False  # Set to True if your MISP has a valid SSL certificate

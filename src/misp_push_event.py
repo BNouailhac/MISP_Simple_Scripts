d6995dc81ba95f24e6a0f2f00cee448b4c70a3a8
Baptiste Nouailhac <baptiste.nouailhac@epitech.eu>
2024-10-22 17:35:00 +0200

    Update misp_push_event.py

--- src/misp_push_event.py
+++ src/misp_push_event.py
@@ -4,10 +4,10 @@ from pymisp import PyMISP
 from conf.misp_conf import misp_url, misp_user_key, misp_verifycert
 
 def load_json_files():
-    #Prend tous les fichiers dans le dossier feeds
+    # Grab all the files in the feeds folder
     json_files = glob.glob("./feeds/**/*.json")
     events = []
-    #Pour chaque fichier les importes dans le MISP local
+    # For each file imported into the local MISP
     for json_file in json_files:
         if (json_file != "manifest.json"):
             with open(json_file, 'r') as f:
